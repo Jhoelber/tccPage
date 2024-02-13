641850305420656ecff3b4d625422ff2bea912f4
Jhoelber <116396136+Jhoelber@users.noreply.github.com>
2024-02-13 19:17:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ sr.reveal('.fraseDentroGrid', {
     distance: '90px'
 
 });
+
+sr.reveal('.aparecer', {
+    rotate: { x: 0, y: 40, z: 0 },
+    duration: 1500,
+    distance: '90px'
+
+});
+
 sr.reveal('.imagemDentroGRID', {
     rotate: { x: 0, y: 40, z: 0 },
     duration: 1000,
